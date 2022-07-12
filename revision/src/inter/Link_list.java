6e3c238779e0161725d178d41cd7c1431c19ea13
Lrssrrb <99732603+Lrssrrb@users.noreply.github.com>
2022-07-12 20:47:35 +0530

    day 10

---
+++ revision/src/inter/Link_list.java
@@ -0,0 +1,18 @@
+package inter;
+
+//import java.util.LinkedList;
+
+//import org.w3c.dom.Node;
+
+public class Link_list {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Link_list lst = new Link_list();
+		Node a = new Node(1);
+		Node sNode = new Node(2);
+		a.next = sNode;
+		System.out.println(a);
+	}
+
+}
